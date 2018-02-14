fa958e59fcb3a377f6edf64788b4ea5f5eea0240
Bjørn Eskildsen <bjes@md-man.biz>
2018-02-14 18:21:23 +0100

    initial

---
+++ app/src/main/java/com/bear_vision/simplefirmata/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bear_vision.simplefirmata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
