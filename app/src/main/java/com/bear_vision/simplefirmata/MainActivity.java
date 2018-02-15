d68f95983746a67fbbfa84385a59547bd3f4e0c3
BeerBear85 <bear.eskildsen@gmail.com>
2018-02-15 22:49:23 +0100

    working UI

--- app/src/main/java/com/bear_vision/simplefirmata/MainActivity.java
+++ app/src/main/java/com/bear_vision/simplefirmata/MainActivity.java
@@ -2,6 +2,8 @@ package com.bear_vision.simplefirmata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendServoCommand(View arg_view) {
+        // Do something in response to button
+        Log.d("MainActivity", "sendServoCommand()");
+    }
+
+    public void sendLEDCommand(View arg_view) {
+        // Do something in response to button
+        Log.d("MainActivity", "sendLEDCommand()");
     }
 }
